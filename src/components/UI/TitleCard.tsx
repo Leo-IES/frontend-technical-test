a645800d857a2b84116d8537e0e7f2d42d1a2f9d
Ivan Leobardo Estrada Salinas <ivan.estrada@comsys.mx>
2024-03-09 16:04:58 -0700

    added loading to title cards

--- src/components/UI/TitleCard.tsx
+++ src/components/UI/TitleCard.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, CircularProgress } from "@mui/material";
 import { useEffect, useState } from "react";
 import bgPlaceHolder from "@Assets/placeholder.png";
 
@@ -15,6 +15,7 @@ export const TitleCard = ({
     error: false,
     url: background,
   });
+  const [loading, setLoading] = useState(true);
   const img = new Image();
   img.src = background;
 
@@ -25,34 +26,60 @@ export const TitleCard = ({
     });
   }, [background]);
 
+  img.onload = function () {
+    setbackgroundURL({
+      error: false,
+      url: background,
+    });
+    setLoading(false);
+  };
+
   img.onerror = function () {
     setbackgroundURL({
       error: true,
       url: bgPlaceHolder,
     });
+    setLoading(false);
   };
   const { url, error } = backgroundURL;
 
   return (
-    <Box onClick={onclick}>
-      <Box
-        sx={{
-          backgroundColor: "rgba(0, 0, 0, 0.9)",
-          backgroundImage: `url(${url})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          "&:hover": {
-            backgroundColor: "rgba(0, 0, 0, 0.5)",
-            border: "1px solid white",
-          },
-        }}
-        className="flex justify-center items-center text-center text-white w-36 h-48"
-      >
-        {
-            error ? title.toUpperCase() : null
-        }
-      </Box>
-      <Box>Program {title}</Box>
-    </Box>
+    <>
+      {loading ? (
+        <Box
+          sx={{
+            backgroundColor: "rgba(0, 0, 0, 0.6)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            "&:hover": {
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              border: "1px solid white",
+            },
+          }}
+          className="flex justify-center items-center text-center text-white w-36 h-48"
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Box onClick={onclick}>
+          <Box
+            sx={{
+              backgroundColor: "rgba(0, 0, 0, 0.9)",
+              backgroundImage: `url(${url})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              "&:hover": {
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+                border: "1px solid white",
+              },
+            }}
+            className="flex justify-center items-center text-center text-white w-36 h-48"
+          >
+            {error ? title.toUpperCase() : null}
+          </Box>
+          <Box>Program {title}</Box>
+        </Box>
+      )}
+    </>
   );
 };
