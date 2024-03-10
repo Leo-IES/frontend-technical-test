299799b5ac89d9da3d83e23cfeb73d4511eae4d0
Ivan Leobardo Estrada Salinas <ivan.estrada@comsys.mx>
2024-03-09 17:42:54 -0700

    fixed footer position

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -10,7 +10,7 @@ export const MainLayout = ({ children }:any) => {
       <Box className="main-Content">
         {children}
       </Box>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
