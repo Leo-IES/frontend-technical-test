0879aec3e80a8d796c5b9983f449538b50ccbe9c
Ivan Leobardo Estrada Salinas <ivan.estrada@comsys.mx>
2024-03-11 16:37:31 -0700

    fixed unit test for main layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,15 +1,20 @@
 import { NavBar } from "@Components/UI/NavBar";
 import { Footer } from "@Components/UI/Footer";
 import { Box } from "@mui/material";
+import { ReactNode } from "react";
 import "../../index.css";
 
-export const MainLayout = ({ children }: any) => {
+interface AuxProps {
+  children?: ReactNode;
+}
+
+export const MainLayout = ({ children }: AuxProps) => {
   return (
     <Box className="relative min-h-screen">
       <Box className="pb-96 sm:pb-48 ">
         <NavBar />
         <Box className="main-Content">{children}</Box>
-      <Box className="pb-60 sm:pb-0 "/>
+        <Box className="pb-60 sm:pb-0 " />
       </Box>
       <Footer />
     </Box>
